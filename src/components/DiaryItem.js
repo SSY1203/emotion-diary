94dba324ceb7deafd62a6d99e702ea0aaf0d27d4
SSY1203 <tlstjsdud566@gmail.com>
2023-03-08 14:01:41 +0900

    [feature] 리스트에 존재하는 기능 구현

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,19 +1,33 @@
 import classNames from 'classnames';
+import { useNavigate } from 'react-router-dom';
 import Button from './Button';
 
 const DiaryItem = ({ id, content, emotion, date }) => {
+  const navigate = useNavigate();
   const strDate = new Date(parseInt(date)).toLocaleDateString();
+
+  const goDetail = () => {
+    navigate(`/diary/${id}`);
+  };
+
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
+
   return (
     <li className="DiaryItem" key={id}>
-      <div className={classNames('emotion_img_wrapper', `emotion_img_wrapper_${emotion}`)}>
+      <div
+        onClick={goDetail}
+        className={classNames('emotion_img_wrapper', `emotion_img_wrapper_${emotion}`)}
+      >
         <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} alt="감정 이미지" />
       </div>
-      <div className="info_wrapper">
+      <div onClick={goDetail} className="info_wrapper">
         <div className="diary_date">{strDate}</div>
         <div className="diary_content_preview">{content.slice(0, 25)}</div>
       </div>
       <div className="button_wrapper">
-        <Button text={'수정하기'} />
+        <Button onClick={goEdit} text={'수정하기'} />
       </div>
     </li>
   );
