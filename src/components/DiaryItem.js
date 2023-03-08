b2e10f10900bdf6164f1e65b06ad78bb7ca6cd00
SSY1203 <tlstjsdud566@gmail.com>
2023-03-08 13:55:08 +0900

    [feature] 리스트 날짜, 컨텐츠 마크업

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,12 +1,16 @@
 import classNames from 'classnames';
 
 const DiaryItem = ({ id, content, emotion, date }) => {
+  const strDate = new Date(parseInt(date)).toLocaleDateString();
   return (
     <li className="DiaryItem" key={id}>
       <div className={classNames('emotion_img_wrapper', `emotion_img_wrapper_${emotion}`)}>
         <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} alt="감정 이미지" />
       </div>
-      <div></div>
+      <div className="info_wrapper">
+        <div className="diary_date">{strDate}</div>
+        <div className="diary_content_preview">{content.slice(0, 25)}</div>
+      </div>
       <div></div>
     </li>
   );
