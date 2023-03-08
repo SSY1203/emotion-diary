4e31f22df49d57f968cbcf95741030cd3bb1c5f6
SSY1203 <tlstjsdud566@gmail.com>
2023-03-08 09:38:28 +0900

    [refactor] 불필요한 파일 제거

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
-import RouteTest from './components/RouteTest';
 
 function App() {
   return (
@@ -17,7 +16,6 @@ function App() {
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouteTest />
       </div>
     </BrowserRouter>
   );
