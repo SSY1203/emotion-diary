4d8b954068c5fc0bc8c8598845620368b78b87a3
SSY1203 <tlstjsdud566@gmail.com>
2023-03-08 13:27:30 +0900

    [feature] 리스트를  option에 따른 정렬 구현

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,10 +1,63 @@
-const DiaryList = ({ diaryList }) => {
+import { useState } from 'react';
+
+const sortOptionList = [
+  { value: 'lastest', name: '최신순' },
+  { value: 'oldest', name: '오래된 순' },
+];
+
+const filterOptionList = [
+  { value: 'all', name: '전부 다' },
+  { value: 'good', name: '좋은 감정만' },
+  { value: 'bad', name: '안 좋은 감정만' },
+];
+
+const ControlMenu = ({ value, onChange, optionList }) => {
   return (
-    <ul>
-      {diaryList.map(item => (
-        <li key={item.id}>{item.content}</li>
+    <select value={value} onChange={e => onChange(e.target.value)}>
+      {optionList.map((item, index) => (
+        <option key={index} value={item.value}>
+          {item.name}
+        </option>
       ))}
-    </ul>
+    </select>
+  );
+};
+
+const DiaryList = ({ diaryList }) => {
+  const [sortType, setSortType] = useState('lastest');
+  const [filter, setFilter] = useState('all');
+
+  const getProcessedDiaryList = () => {
+    const filterCallBack = item =>
+      filter === 'good' ? parseInt(item.emotion) >= 3 : parseInt(item.emotion) < 3;
+
+    const compare = (a, b) => {
+      if (sortType === 'lastest') {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    };
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+    const filteredList =
+      filter === 'all' ? copyList : copyList.filter(item => filterCallBack(item));
+    const sortedList = filteredList.sort(compare);
+
+    return sortedList;
+  };
+
+  return (
+    <div>
+      <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList} />
+      <ControlMenu value={filter} onChange={setFilter} optionList={filterOptionList} />
+      <ul>
+        {getProcessedDiaryList().map(item => (
+          <li key={item.id}>
+            {item.content} {item.emotion}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
