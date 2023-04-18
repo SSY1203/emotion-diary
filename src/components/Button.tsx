6234c83b927c89ace6cedf873661f9b601c0c2ca
SSY1203 <tlstjsdud566@gmail.com>
2023-04-18 12:35:28 +0900

    [modify] Button 파일 타입스크립트로 변경

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,21 @@
 import classNames from 'classnames';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ text, type, onClick }) => {
-  const buttonType = ['positive', 'negative'].includes(type) ? type : 'default';
+interface ButtonProp {
+  text: string;
+  type: string;
+  onClick: () => void;
+}
+
+const Button = ({ text, type, onClick }: ButtonProp) => {
+  const buttonType: string = ['positive', 'negative'].includes(type) ? type : 'default';
 
   return (
     <button className={classNames('MyButton', `MyButton_${buttonType}`)} onClick={onClick}>
       {text}
     </button>
   );
-};
-
-Button.propTypes = {
-  type: PropTypes.string,
-  text: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
 };
 
 Button.defaultProps = {
