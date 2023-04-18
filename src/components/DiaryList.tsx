de4beaf7ba1b3c20d81435368308ca46370e20c7
SSY1203 <tlstjsdud566@gmail.com>
2023-04-18 12:34:09 +0900

    [modify] DiaryList 파일 타입스크립트로 변경

--- src/components/DiaryList.tsx
+++ src/components/DiaryList.tsx
@@ -2,22 +2,38 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from './Button';
 import DiaryItem from './DiaryItem';
+import { DiaryDataType } from './../App';
 
-const sortOptionList = [
+interface OptionType {
+  value: string;
+  name: string;
+}
+
+const sortOptionList: OptionType[] = [
   { value: 'lastest', name: '최신순' },
   { value: 'oldest', name: '오래된 순' },
 ];
 
-const filterOptionList = [
+const filterOptionList: OptionType[] = [
   { value: 'all', name: '전부 다' },
   { value: 'good', name: '좋은 감정만' },
   { value: 'bad', name: '안 좋은 감정만' },
 ];
 
-const ControlMenu = React.memo(({ value, onChange, optionList }) => {
+interface ControlMenuProp {
+  value: string;
+  onChange: (value: string) => void;
+  optionList: OptionType[];
+}
+
+const ControlMenu = React.memo(({ value, onChange, optionList }: ControlMenuProp) => {
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = e.target;
+    onChange(value);
+  };
   return (
-    <select className="ControlMenu" value={value} onChange={e => onChange(e.target.value)}>
-      {optionList.map((item, index) => (
+    <select className="ControlMenu" value={value} onChange={handleChange}>
+      {optionList.map((item: OptionType, index: number) => (
         <option key={index} value={item.value}>
           {item.name}
         </option>
@@ -26,26 +42,30 @@ const ControlMenu = React.memo(({ value, onChange, optionList }) => {
   );
 });
 
-const DiaryList = ({ diaryList }) => {
+interface DiaryListProp {
+  diaryList: DiaryDataType[];
+}
+
+const DiaryList = ({ diaryList }: DiaryListProp) => {
   const navigate = useNavigate();
-  const [sortType, setSortType] = useState('lastest');
-  const [filter, setFilter] = useState('all');
+  const [sortType, setSortType] = useState<string>('lastest');
+  const [filter, setFilter] = useState<string>('all');
 
   const getProcessedDiaryList = () => {
-    const filterCallBack = item =>
-      filter === 'good' ? parseInt(item.emotion) <= 3 : parseInt(item.emotion) > 3;
+    const filterCallBack = (item: DiaryDataType) =>
+      filter === 'good' ? item.emotion <= 3 : item.emotion > 3;
 
-    const compare = (a, b) => {
+    const compare = (a: DiaryDataType, b: DiaryDataType) => {
       if (sortType === 'lastest') {
-        return parseInt(b.date) - parseInt(a.date);
+        return b.date - a.date;
       } else {
-        return parseInt(a.date) - parseInt(b.date);
+        return a.date - b.date;
       }
     };
     const copyList = JSON.parse(JSON.stringify(diaryList));
-    const filteredList =
-      filter === 'all' ? copyList : copyList.filter(item => filterCallBack(item));
-    const sortedList = filteredList.sort(compare);
+    const filteredList: DiaryDataType[] =
+      filter === 'all' ? copyList : copyList.filter((item: DiaryDataType) => filterCallBack(item));
+    const sortedList: DiaryDataType[] = filteredList.sort(compare);
 
     return sortedList;
   };
